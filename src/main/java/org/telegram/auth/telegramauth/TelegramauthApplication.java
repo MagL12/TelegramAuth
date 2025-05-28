463f855be352bd0e9f35c2028a9208c501bd7910
MagL12 <cool.magl@yandex.ru>
2025-05-28 19:25:49 +0700

    init

---
+++ src/main/java/org/telegram/auth/telegramauth/TelegramauthApplication.java
@@ -0,0 +1,13 @@
+package org.telegram.auth.telegramauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramauthApplication.class, args);
+	}
+
+}
